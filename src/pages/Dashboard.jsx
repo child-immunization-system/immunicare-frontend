85445d33b1668f0c41ee9988815d9c3227eef33e
Your name <omowunmidaud1@gmail.com>
2024-06-29 08:35:27 +0100

    setup dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,20 @@
+import { Header } from "../components/dashboard/Header"
+import { Main } from "../components/dashboard/Main"
 import Sidemenu from "../components/dashboard/SideMenu"
-
 
 const Dashboard = () => {
   return (
     <div className="flex">
 
         <Sidemenu />
+
+        <section className="flex flex-col">
+
+          <Header />
+
+          <Main />
+
+        </section>
 
     </div>
   )
